efac09ff4ae8629f0a3abae33463cb6a6246a98e
Shadowpigkiller <119260016+Shadowpigkiller@users.noreply.github.com>
2025-11-28 17:15:43 -0500

    update

--- KNN-Classification.py
+++ KNN-Classification.py
@@ -18,17 +18,18 @@ train_data2 = pd.read_csv("Data/classification/TrainData2.txt", sep='\t', header
 train_labels2 = pd.read_csv("Data/classification/TrainLabel2.txt", header=None).values.ravel()
 test_data2 = pd.read_csv("Data/classification/TestData2.txt", sep='\t', header=None)
 #train_data2 = train_data2.apply(pd.to_numeric, errors='coerce')
-print(train_data2.dtypes)
+#print(train_data2.dtypes)
 #Set 3
 train_data3 = pd.read_csv("Data/classification/TrainData3.txt", sep='\t', header=None)
 train_labels3 = pd.read_csv("Data/classification/TrainLabel3.txt", header=None).values.ravel()
 test_data3 = pd.read_csv("Data/classification/TestData3.txt", sep='\t', header=None)
+#print(train_data3.dtypes)
 
 #Set 4
 train_data4 = pd.read_csv("Data/classification/TrainData4.txt", sep='\t', header=None)
 train_labels4 = pd.read_csv("Data/classification/TrainLabel4.txt", header=None).values.ravel()
 test_data4 = pd.read_csv("Data/classification/TestData4.txt", sep='\t', header=None)
-
+print(train_data4.dtypes)
 #Set 1
 #train_data1.replace(1.00000000000000e+99, np.nan, inplace=True)
 #test_data1.replace(1.00000000000000e+99, np.nan, inplace=True)
@@ -72,3 +73,24 @@ pred_scaled2 = pipe_scaled2.predict(test_data2)
 print(f"\nSet 2: KNN without scaling - Mean CV Accuracy: {scores_no_scale2.mean():.4f}")
 print(f"Set 2: KNN with scaling - Mean CV Accuracy: {scores_scaled2.mean():.4f}")
 '''
+
+#Set 3
+train_data3.replace(1.00000000000000e+99, np.nan, inplace=True)
+test_data3.replace(1.00000000000000e+99, np.nan, inplace=True)
+imputer = SimpleImputer(strategy='mean')
+train_data3 = pd.DataFrame(imputer.fit_transform(train_data3))
+test_data3 = pd.DataFrame(imputer.transform(test_data3))
+knn_no_scale = KNeighborsClassifier(n_neighbors=5)
+scores_no_scale = cross_val_score(knn_no_scale, train_data3, train_labels3, cv=5)
+# Fit on all training data and predict
+knn_no_scale.fit(train_data3, train_labels3)
+pred_no_scale = knn_no_scale.predict(test_data3)
+pipe_scaled = Pipeline([
+    ('scaler', StandardScaler()),
+    ('knn', KNeighborsClassifier(n_neighbors=5))
+])
+scores_scaled = cross_val_score(pipe_scaled, train_data3, train_labels3, cv=5)
+pipe_scaled.fit(train_data3, train_labels3)
+pred_scaled = pipe_scaled.predict(test_data3)
+print(f"\nSet 1: KNN without scaling - Mean CV Accuracy: {scores_no_scale.mean():.4f}")
+print(f"Set 1: KNN with scaling - Mean CV Accuracy: {scores_scaled.mean():.4f}")
