7a4616e8aa64aaa7d0644c6440689085b9c4635f
ThomasNVu <139417064+ThomasNVu@users.noreply.github.com>
2025-12-14 17:56:41 -0500

    Added testing data

--- Naive-Bayes.py
+++ Naive-Bayes.py
@@ -1,24 +1,25 @@
 import pandas as pd
-import re
 from sklearn.naive_bayes import MultinomialNB, ComplementNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 import csv
 
 
 TRAIN_OPTION = "3"
 FILE_TRAIN1 = "Data/spam/spam_train1.csv"
 FILE_TRAIN2 = "Data/spam/spam_train2.csv"
-MODEL = "1"
+FILE_TEST = "Data/spam/spam_test.csv"
+MODEL = "2"
+TRAINING_DATA = True
 
 
 def label_finder(path):
-    with open(path, newline='', encoding='utf-8') as file:
+    with open(path, newline="", encoding="utf-8") as file:
         reader = csv.reader(file)
         header = next(reader, None)
         row = next(reader, None)
-        
+
         for col, value in enumerate(row):
             if value.strip().lower() == "ham" or value.strip().lower() == "spam":
                 return col
@@ -27,7 +28,9 @@ def label_finder(path):
 def load_data(path):
     label_col = label_finder(path)
     text_col = label_col + 1
-    df = pd.read_csv(path, usecols=[label_col, text_col], names=["label", "text"], header=0)
+    df = pd.read_csv(
+        path, usecols=[label_col, text_col], names=["label", "text"], header=0
+    )
     return df
 
 
@@ -50,28 +53,11 @@ X_train, X_test, y_train, y_test = train_test_split(
     train["text"],
     train["label"],
     test_size=0.2,
-    random_state=42,
     shuffle=True,
 )
 
 
-# Cleans the Data
-# def clean(text):
-#     text = str(text).lower()
-#     text = re.sub(r"http\S+|www\S+|https\S+", " ", text)
-#     text = re.sub(r"\S+@\S+", " ", text)
-#     text = re.sub(r"[^a-z\s]", " ", text)
-#     tokens = text.split()
-#     return [t for t in tokens if len(t) > 2]
-
-
-# vectorizer = CountVectorizer(
-#     analyzer=clean,
-#     token_pattern=None,
-#     min_df=2,
-# )
-
-vectorizer = CountVectorizer(
+vectorizer = TfidfVectorizer(
     lowercase=True,
     stop_words="english",
     token_pattern=r"\b\w+\b",  # Only Letters and Numbers can exist
@@ -91,7 +77,22 @@ elif MODEL == "2":
 
 clf.fit(X_train_vec, y_train)
 
-# Evaluate
-pred = clf.predict(X_test_vec)
-print(f"Accuracy:, {accuracy_score(y_test, pred) *100:.2f}%")
-print(classification_report(y_test, pred ,zero_division=0))
+if TRAINING_DATA:
+    text = X_test
+    vec = X_test_vec
+    labels = y_test
+else:
+    spam_test_df = pd.read_csv(FILE_TEST, header=0)
+    text = spam_test_df.iloc[:, 0]
+    vec = vectorizer.transform(text)
+    labels = None
+
+# Predict
+pred = clf.predict(vec)
+
+print(f"Accuracy:, {accuracy_score(labels, pred) *100:.2f}%")
+print(classification_report(labels, pred ,zero_division=0))
+
+# Export predictions
+results = pd.DataFrame({"Prediction": pred})
+results.replace({"spam": 1, "ham": 0}).to_csv("NBTestResults.txt", index=False, header=False)
